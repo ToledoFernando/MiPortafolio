99ba666101fb2b36cdb2b9bfda2a3d6b02d813d2
ToledoFernando <elvioladorxx3@gmail.com>
2022-12-08 03:16:17 -0300

    prueba navbar Responsive

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
-import './NavBar.scss';
+import React from "react";
+import "./NavBar.scss";
 
 function NavBar() {
-
   const navBar = React.useRef();
+  const [active, setActive] = React.useState(false);
 
-  React.useEffect(()=>{
-    window.addEventListener('scroll',(e)=>{
-      if(Math.round(e.srcElement.scrollingElement.scrollTop) > 30) navBar.current.setAttribute('style','background-color: #000')
-      if(Math.round(e.srcElement.scrollingElement.scrollTop) < 30) navBar.current.setAttribute('style','background-color: #00000000')
-    })
-  })
+  React.useEffect(() => {
+    window.addEventListener("scroll", (e) => {
+      if (Math.round(e.srcElement.scrollingElement.scrollTop) > 30)
+        navBar.current.setAttribute("style", "background-color: #000");
+      if (Math.round(e.srcElement.scrollingElement.scrollTop) < 30)
+        navBar.current.setAttribute("style", "background-color: #00000000");
+    });
+  });
 
   return (
     <nav ref={navBar}>
-      <div className='logo'>
-        <p>{'< />'}</p>
+      <div
+        className={active ? "menuAct" : "menu"}
+        onClick={() => setActive(!active)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className="logo">
+        <p>{"< />"}</p>
         <p>FullStack Developer_</p>
       </div>
-      <div className='opt'>
+      <div className={active ? "menuAv" : "opt"}>
         <ul>
           <li>Inicio</li>
           <li>About</li>
@@ -27,7 +37,7 @@ function NavBar() {
         </ul>
       </div>
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
